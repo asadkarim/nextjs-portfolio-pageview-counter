7c0ef0c250c23dcdba58db3693aa519bd2396fa5
Asad Karim <asadkarim.fc@gmail.com>
2025-08-14 10:42:28 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,8 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+          I am a Microsoft Dynamics 365 Business Central consultant | Emerging Copilot Studio & Power Automate Practitioner.
+
         </h2>
       </div>
     </div>
